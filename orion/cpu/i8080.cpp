ffe4e743463310df59053f8fe219d65c3849696f
Артём Оконечников <temaweb@yandex.ru>
2020-10-31 03:27:58 +0300

    0x1 - 0x8

--- orion/cpu/i8080.cpp
+++ orion/cpu/i8080.cpp
@@ -20,14 +20,160 @@ i8080::i8080() : reg { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
         { "NOP",     4,        &i8080::NOP,      &i8080::IMP },
         { "LXI",     10,       &i8080::LXIB,     &i8080::IMM },
         { "STAX",    7,        &i8080::STAXB,    &i8080::IMP },
+        { "INX",     5,        &i8080::INXB,     &i8080::IMP },
+        { "INR",     5,        &i8080::INRR,     &i8080::IMP },
+        { "DCR",     5,        &i8080::DCRR,     &i8080::IMP },
+        { "MVI",     5,        &i8080::MVIR,     &i8080::IMM },
+        { "RLC",     4,        &i8080::RLC,      &i8080::IMP },
+        { "XXX",     1,        &i8080::XXX,      &i8080::IMP },
+        { "DAD",     10,       &i8080::DADB,     &i8080::IMP },
+        { "LDAX",    7,        &i8080::LDAXB,    &i8080::IND },
+        { "DCX",     5,        &i8080::DCXB,     &i8080::IMP },
+        { "INR",     5,        &i8080::INRR,     &i8080::IMP },
+        { "DCR",     5,        &i8080::DCRR,     &i8080::IMP },
+        { "MVI",     7,        &i8080::MVIR,     &i8080::IMM },
+        { "RRC",     4,        &i8080::RRC,      &i8080::IMP },
         
         // 0x1 - 0xF
         
         { "XXX",     4,        &i8080::XXX,      &i8080::IMP },
         { "LXI",     10,       &i8080::LXID,     &i8080::IMM },
-        { "STAX",    7,        &i8080::STAXD,    &i8080::IMP }
+        { "STAX",    7,        &i8080::STAXD,    &i8080::IMP },
+        { "INX",     5,        &i8080::INXD,     &i8080::IMP },
+        { "INR",     5,        &i8080::INRR,     &i8080::IMP },
+        { "DCR",     5,        &i8080::DCRR,     &i8080::IMP },
+        { "MVI",     5,        &i8080::MVIR,     &i8080::IMM },
+        { "RAL",     4,        &i8080::RAL,      &i8080::IMP },
+        { "XXX",     1,        &i8080::XXX,      &i8080::IMP },
+        { "DAD",     10,       &i8080::DADD,     &i8080::IMP },
+        { "LDAX",    7,        &i8080::LDAXD,    &i8080::IND },
+        { "DCX",     5,        &i8080::DCXD,     &i8080::IMP },
+        { "INR",     5,        &i8080::INRR,     &i8080::IMP },
+        { "DCR",     5,        &i8080::DCRR,     &i8080::IMP },
+        { "MVI",     7,        &i8080::MVIR,     &i8080::IMM },
+        { "RAR",     4,        &i8080::RAR,      &i8080::IMP },
         
+        // 0x2 - 0xF
         
+        { "XXX",     4,        &i8080::XXX,      &i8080::IMP },
+        { "LXI",     10,       &i8080::LXIH,     &i8080::IMM },
+        { "SHLD",    16,       &i8080::SHLD,     &i8080::DIR },
+        { "INX",     5,        &i8080::INXH,     &i8080::IMP },
+        { "INR",     5,        &i8080::INRR,     &i8080::IMP },
+        { "DCR",     5,        &i8080::DCRR,     &i8080::IMP },
+        { "MVI",     5,        &i8080::MVIR,     &i8080::IMM },
+        { "DAA",     4,        &i8080::DAA,      &i8080::IMP },
+        { "DAD",     10,       &i8080::DADH,     &i8080::IMP },
+        { "LHLD",    16,       &i8080::LHLD,     &i8080::DIR },
+        { "DCX",     5,        &i8080::DCXH,     &i8080::IMP },
+        { "INR",     5,        &i8080::INRR,     &i8080::IMP },
+        { "DCR",     5,        &i8080::DCRR,     &i8080::IMP },
+        { "MVI",     7,        &i8080::MVIR,     &i8080::IMM },
+        { "CMA",     4,        &i8080::CMA,      &i8080::IMP },
+        
+        // 0x3 - 0xF
+        
+        { "XXX",     4,        &i8080::XXX,      &i8080::IMP },
+        { "LXI",     10,       &i8080::LXI,      &i8080::IMM },
+        { "STA",     13,       &i8080::STA,      &i8080::DIR },
+        { "INX",     5,        &i8080::INX,      &i8080::IMP },
+        { "INR",     10,       &i8080::INRM,     &i8080::HLM },
+        { "DCR",     10,       &i8080::DCRM,     &i8080::HLM },
+        { "MVI",     10,       &i8080::MVIM,     &i8080::IMM },
+        { "STC",     4,        &i8080::STC,      &i8080::IMP },
+        { "XXX",     1,        &i8080::XXX,      &i8080::IMP },
+        { "DAD",     10,       &i8080::DAD,      &i8080::IMP },
+        { "LDA",     16,       &i8080::LDA,      &i8080::DIR },
+        { "DCX",     13,       &i8080::DCX,      &i8080::IMP },
+        { "INR",     5,        &i8080::INRR,     &i8080::IMP },
+        { "DCR",     5,        &i8080::DCRR,     &i8080::IMP },
+        { "MVI",     7,        &i8080::MVIR,     &i8080::IMM },
+        { "CMC",     4,        &i8080::CMC,      &i8080::IMP },
+        
+        // 0x4 - 0xF
+        
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     7,        &i8080::MOVRM,     &i8080::HLM },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     7,        &i8080::MOVRM,     &i8080::HLM },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        
+        // 0x5 - 0xF
+        
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     7,        &i8080::MOVRM,     &i8080::HLM },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        { "MOV",     7,        &i8080::MOVRM,     &i8080::HLM },
+        { "MOV",     5,        &i8080::MOVRR,     &i8080::IMP },
+        
+        // 0x6 - 0xF
+        
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     7,        &i8080::MOVRM,      &i8080::HLM },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     7,        &i8080::MOVRM,      &i8080::HLM },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        
+        // 0x7 - 0xF
+        
+        { "MOV",     7,        &i8080::MOVMR,      &i8080::HLM },
+        { "MOV",     7,        &i8080::MOVMR,      &i8080::HLM },
+        { "MOV",     7,        &i8080::MOVMR,      &i8080::HLM },
+        { "MOV",     7,        &i8080::MOVMR,      &i8080::HLM },
+        { "MOV",     7,        &i8080::MOVMR,      &i8080::HLM },
+        { "MOV",     7,        &i8080::MOVMR,      &i8080::HLM },
+        { "MOV",     7,        &i8080::HLT,        &i8080::IMP },
+        { "MOV",     7,        &i8080::MOVMR,      &i8080::HLM },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     7,        &i8080::MOVRM,      &i8080::HLM },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        
+        // 0x8 - 0xF
+        
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
+        { "MOV",     5,        &i8080::MOVRR,      &i8080::IMP },
     };
 }
 
@@ -44,7 +190,7 @@ void i8080::clock()
     // Read operation code
     op = read(pc);
     
-    // Increment counter
+    // Increment program counter
     pc++;
     
     // Set min program cycles
@@ -107,20 +253,6 @@ void i8080::HLM()
     uint16_t hi = reg[H];
     
     address = (hi << 8) | lo;
-}
-
-#pragma mark -
-
-uint8_t i8080::XXX()
-{
-    return 0;
-}
-
-// Code: NOP
-// Operation: No-operation
-uint8_t i8080::NOP()
-{
-    return 0;
 }
 
 #pragma mark -
@@ -325,5 +457,165 @@ uint8_t i8080::XCHG()
     exchange(reg[D], reg[H]);
     exchange(reg[E], reg[L]);
     
+    return 0;
+}
+
+#pragma mark -
+#pragma mark Stack operations
+
+uint8_t i8080::PUSHB () { return 0; }
+uint8_t i8080::PUSHD () { return 0; }
+uint8_t i8080::PUSHH () { return 0; }
+uint8_t i8080::PUSH  () { return 0; }  // PSW
+uint8_t i8080::POPB  () { return 0; }
+uint8_t i8080::POPD  () { return 0; }
+uint8_t i8080::POPH  () { return 0; }
+uint8_t i8080::POP   () { return 0; }  // PSW
+uint8_t i8080::XTHL  () { return 0; }
+uint8_t i8080::SPHL  () { return 0; }
+uint8_t i8080::LXI   () { return 0; }  // SP
+uint8_t i8080::INX   () { return 0; }  // SP
+uint8_t i8080::DCX   () { return 0; }  // SP
+
+#pragma mark -
+#pragma mark Jump
+
+uint8_t i8080::JMP  () { return 0; }
+uint8_t i8080::JC   () { return 0; }
+uint8_t i8080::JNC  () { return 0; }
+uint8_t i8080::JZ   () { return 0; }
+uint8_t i8080::JNZ  () { return 0; }
+uint8_t i8080::JP   () { return 0; }
+uint8_t i8080::JM   () { return 0; }
+uint8_t i8080::JPE  () { return 0; }
+uint8_t i8080::JPO  () { return 0; }
+uint8_t i8080::PCHL () { return 0; }
+
+#pragma mark -
+#pragma mark Call
+
+uint8_t i8080::CALL () { return 0; }
+uint8_t i8080::CC   () { return 0; }
+uint8_t i8080::CNC  () { return 0; }
+uint8_t i8080::CZ   () { return 0; }
+uint8_t i8080::CNZ  () { return 0; }
+uint8_t i8080::CP   () { return 0; }
+uint8_t i8080::CM   () { return 0; }
+uint8_t i8080::CPE  () { return 0; }
+uint8_t i8080::CPO  () { return 0; }
+
+#pragma mark -
+#pragma mark Return
+
+uint8_t i8080::RET  () { return 0; }
+uint8_t i8080::RC   () { return 0; }
+uint8_t i8080::RNC  () { return 0; }
+uint8_t i8080::RZ   () { return 0; }
+uint8_t i8080::RNZ  () { return 0; }
+uint8_t i8080::RP   () { return 0; }
+uint8_t i8080::RM   () { return 0; }
+uint8_t i8080::RPE  () { return 0; }
+uint8_t i8080::RPO  () { return 0; }
+
+#pragma mark -
+#pragma mark Restart
+
+uint8_t RST  ()
+{
+    return 0;
+}
+
+#pragma mark -
+#pragma mark Increment and decrement
+
+uint8_t i8080::INRR () { return 0; }
+uint8_t i8080::INRM () { return 0; }
+uint8_t i8080::DCRR () { return 0; }
+uint8_t i8080::DCRM () { return 0; }
+uint8_t i8080::INXB () { return 0; }
+uint8_t i8080::INXD () { return 0; }
+uint8_t i8080::INXH () { return 0; }
+uint8_t i8080::DCXB () { return 0; }
+uint8_t i8080::DCXD () { return 0; }
+uint8_t i8080::DCXH () { return 0; }
+
+#pragma mark -
+#pragma mark Add
+
+uint8_t i8080::ADDR () { return 0; }
+uint8_t i8080::ADDM () { return 0; }
+uint8_t i8080::ADCR () { return 0; }
+uint8_t i8080::ADCM () { return 0; }
+uint8_t i8080::ADI  () { return 0; }
+uint8_t i8080::ACI  () { return 0; }
+uint8_t i8080::DADB () { return 0; }
+uint8_t i8080::DADD () { return 0; }
+uint8_t i8080::DADH () { return 0; }
+uint8_t i8080::DAD  () { return 0; }
+
+#pragma mark -
+#pragma mark Substract
+
+uint8_t i8080::SUBR () { return 0; }
+uint8_t i8080::SUBM () { return 0; }
+uint8_t i8080::SBBR () { return 0; }
+uint8_t i8080::SBBM () { return 0; }
+uint8_t i8080::SUI  () { return 0; }
+uint8_t i8080::SBI  () { return 0; }
+
+#pragma mark -
+#pragma mark Logical
+
+uint8_t i8080::ANAR () { return 0; }
+uint8_t i8080::XRAR () { return 0; }
+uint8_t i8080::ORAR () { return 0; }
+uint8_t i8080::CMPR () { return 0; }
+uint8_t i8080::ANAM () { return 0; }
+uint8_t i8080::XRAM () { return 0; }
+uint8_t i8080::ORAM () { return 0; }
+uint8_t i8080::CMPM () { return 0; }
+uint8_t i8080::ANI  () { return 0; }
+uint8_t i8080::XRI  () { return 0; }
+uint8_t i8080::ORI  () { return 0; }
+uint8_t i8080::CPI  () { return 0; }
+
+#pragma mark -
+#pragma mark Rotate
+
+uint8_t i8080::RLC  () { return 0; }
+uint8_t i8080::RRC  () { return 0; }
+uint8_t i8080::RAL  () { return 0; }
+uint8_t i8080::RAR  () { return 0; }
+
+#pragma mark -
+#pragma mark Special
+
+uint8_t i8080::CMA  () { return 0; }
+uint8_t i8080::STC  () { return 0; }
+uint8_t i8080::CMC  () { return 0; }
+uint8_t i8080::DAA  () { return 0; }
+
+#pragma mark -
+#pragma mark I/O
+
+uint8_t i8080::IN   () { return 0; }
+uint8_t i8080::OUT  () { return 0; }
+
+#pragma mark -
+#pragma mark Control
+
+uint8_t i8080::EI   () { return 0; }
+uint8_t i8080::DI   () { return 0; }
+uint8_t i8080::HLT  () { return 0; }
+
+// Code: NOP
+// Operation: No-operation
+uint8_t i8080::NOP()
+{
+    return 0;
+}
+
+uint8_t i8080::XXX()
+{
     return 0;
 }
--- orion/cpu/i8080.hpp
+++ orion/cpu/i8080.hpp
@@ -24,18 +24,18 @@ public:
     
 private:
     
-    uint8_t a = 0x00;       // Accumulator
+    uint8_t  reg[6];            // Registers        (B C D E H L)
     
-    uint8_t reg[6];         // B C D E H L
+    uint8_t  a       = 0x00;    // Accumulator      (A)
+    uint8_t  sr      = 0x00;    // Status register  (S Z - AC - P - C)
     
-    uint16_t sp = 0x0000;   // Stack pointer
-    uint16_t pc = 0x0000;   // Program counter
-  
-    uint8_t  sr = 0x00;     // Status register
-    
-    uint8_t  op     = 0x00; // Operation code
-    uint8_t  cycles = 0x00; // Cycle counter
+    uint8_t  op      = 0x00;    // Operation code
+    uint8_t  cycles  = 0x00;    // Cycle counter
 
+    
+    uint16_t sp      = 0x0000;  // Stack pointer
+    uint16_t pc      = 0x0000;  // Program counter
+    
     uint16_t address = 0x0000;
     
 private:
@@ -58,6 +58,15 @@ private:
         DDD = 0x38
     };
     
+    enum Flags
+    {
+        Sign    = (1 << 7), // S
+        Zero    = (1 << 6), // Z
+        Aux     = (1 << 4), // AC
+        Parity  = (1 << 2), // P
+        Carry   = (1 << 0)  // C
+    };
+    
 // Bus communication
 private:
     
@@ -77,28 +86,20 @@ private:
     
 private:
     
-    uint8_t XXX();
-    uint8_t NOP();
-    
     // Move, Load, Store
     
     uint8_t MOVRR ();
     uint8_t MOVMR ();
     uint8_t MOVRM ();
-    
     uint8_t MVIR  ();
     uint8_t MVIM  ();
-    
     uint8_t LXIB  ();
     uint8_t LXID  ();
     uint8_t LXIH  ();
-    
     uint8_t STAXB ();
     uint8_t STAXD ();
-    
     uint8_t LDAXB ();
     uint8_t LDAXD ();
-    
     uint8_t STA   ();
     uint8_t LDA   ();
     uint8_t SHLD  ();
@@ -111,24 +112,134 @@ private:
     uint8_t PUSHB ();
     uint8_t PUSHD ();
     uint8_t PUSHH ();
-    
-    uint8_t PUSH  ();   // PSW
-    
+    uint8_t PUSH  ();  // PSW
     uint8_t POPB  ();
     uint8_t POPD  ();
     uint8_t POPH  ();
-    
-    uint8_t POP   ();   // PSW
-    
+    uint8_t POP   ();  // PSW
     uint8_t XTHL  ();
     uint8_t SPHL  ();
-    
     uint8_t LXI   ();  // SP
     uint8_t INX   ();  // SP
     uint8_t DCX   ();  // SP
-    
+
     // Jump
     
+    uint8_t JMP  ();
+    uint8_t JC   ();
+    uint8_t JNC  ();
+    uint8_t JZ   ();
+    uint8_t JNZ  ();
+    uint8_t JP   ();
+    uint8_t JM   ();
+    uint8_t JPE  ();
+    uint8_t JPO  ();
+    uint8_t PCHL ();
+    
+    // Call
+    
+    uint8_t CALL ();
+    uint8_t CC   ();
+    uint8_t CNC  ();
+    uint8_t CZ   ();
+    uint8_t CNZ  ();
+    uint8_t CP   ();
+    uint8_t CM   ();
+    uint8_t CPE  ();
+    uint8_t CPO  ();
+    
+    // Return
+    
+    uint8_t RET  ();
+    uint8_t RC   ();
+    uint8_t RNC  ();
+    uint8_t RZ   ();
+    uint8_t RNZ  ();
+    uint8_t RP   ();
+    uint8_t RM   ();
+    uint8_t RPE  ();
+    uint8_t RPO  ();
+    
+    // Restart
+    
+    uint8_t RST  ();
+    
+    // Increment and decrement
+    
+    uint8_t INRR ();
+    uint8_t INRM ();
+    uint8_t DCRR ();
+    uint8_t DCRM ();
+    uint8_t INXB ();
+    uint8_t INXD ();
+    uint8_t INXH ();
+    uint8_t DCXB ();
+    uint8_t DCXD ();
+    uint8_t DCXH ();
+    
+    // Add
+    
+    uint8_t ADDR ();
+    uint8_t ADDM ();
+    uint8_t ADCR ();
+    uint8_t ADCM ();
+    uint8_t ADI  ();
+    uint8_t ACI  ();
+    uint8_t DADB ();
+    uint8_t DADD ();
+    uint8_t DADH ();
+    uint8_t DAD  ();
+    
+    // Substract
+    
+    uint8_t SUBR ();
+    uint8_t SUBM ();
+    uint8_t SBBR ();
+    uint8_t SBBM ();
+    uint8_t SUI  ();
+    uint8_t SBI  ();
+    
+    // Logical
+    
+    uint8_t ANAR ();
+    uint8_t XRAR ();
+    uint8_t ORAR ();
+    uint8_t CMPR ();
+    uint8_t ANAM ();
+    uint8_t XRAM ();
+    uint8_t ORAM ();
+    uint8_t CMPM ();
+    uint8_t ANI  ();
+    uint8_t XRI  ();
+    uint8_t ORI  ();
+    uint8_t CPI  ();
+    
+    // Rotate
+    
+    uint8_t RLC  ();
+    uint8_t RRC  ();
+    uint8_t RAL  ();
+    uint8_t RAR  ();
+    
+    // Special
+    
+    uint8_t CMA  ();
+    uint8_t STC  ();
+    uint8_t CMC  ();
+    uint8_t DAA  ();
+    
+    // I/O
+    
+    uint8_t IN   ();
+    uint8_t OUT  ();
+    
+    // Control
+    
+    uint8_t EI   ();
+    uint8_t DI   ();
+    uint8_t NOP  ();
+    uint8_t HLT  ();
+    uint8_t XXX  ();
     
 private:
     
